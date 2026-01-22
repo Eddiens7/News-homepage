917f34b56478a9a8d755549eb843557d2ec1ac60
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-01-22 11:06:23 -0300

    mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -45,27 +45,33 @@ function App() {
             <div className="popular-topic-bg">
               <img src={retroPcImg} alt="" />
             </div>
-            <h1>01</h1>
-            <h2>Reviving Retro PCs</h2>
-            <p>What happens when old PCs are given modern upgrades? </p>
+            <div className='popular-topic-text'>
+              <h1>01</h1>
+              <h2>Reviving Retro PCs</h2>
+              <p>What happens when old PCs are given modern upgrades? </p>
+            </div>
           </div>
 
           <div className="popular-topic">
             <div className="popular-topic-bg">
               <img src={laptopImg} alt="" />
             </div>
-            <h1>02</h1>
-            <h2>Top 10 Laptops of 2022</h2>
-            <p>Our best picks for various needs and budgets. </p>
+            <div className='popular-topic-text'>
+              <h1>02</h1>
+              <h2>Top 10 Laptops of 2022</h2>
+              <p>Our best picks for various needs and budgets. </p>
+            </div>
           </div>
 
           <div className="popular-topic">
             <div className="popular-topic-bg">
               <img src={gameImg} alt="" />
             </div>
-            <h1>03</h1>
-            <h2>The Growth of Gaming</h2>
-            <p>How the pandemic has sparked fresh opportunities.</p>
+            <div className='popular-topic-text'>
+              <h1>03</h1>
+              <h2>The Growth of Gaming</h2>
+              <p>How the pandemic has sparked fresh opportunities.</p>
+            </div>
           </div>
         </section>
       </main>
